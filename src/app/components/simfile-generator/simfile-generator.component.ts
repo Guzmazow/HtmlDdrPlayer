9936d1a4417a577de2150f50c38e02b386b818a0
Guzmazow <guzmazow@gmail.com>
2023-04-10 03:47:35 +0300

    music predictions using ML and

--- src/app/components/simfile-generator/simfile-generator.component.ts
+++ src/app/components/simfile-generator/simfile-generator.component.ts
@@ -4,7 +4,8 @@ import { checkPeaks, tempoDetect } from '@other/audio';
 import { Log } from '@services/log.service';
 import { WaveformViewComponent } from 'src/app/modules/peaks/waveform-view/waveform-view.component';
 import { AudioClassifier, AudioClassifierResult, FilesetResolver } from '@mediapipe/tasks-audio';
-// import { Rank, Tensor, loadGraphModel, zeros } from '@tensorflow/tfjs'
+import * as tf from '@tensorflow/tfjs'
+import { YamNetTypes } from '@other/yamnet';
 
 @Component({
   selector: 'app-simfile-generator',
@@ -86,7 +87,16 @@ export class SimfileGeneratorComponent implements AfterViewInit {
         });
       });
 
-      var classResults: AudioClassifierResult[] = [];
+      // var classResults: AudioClassifierResult[] = [];
+      var classResults: {
+        timestampMs: number,
+        classifications: {
+          categories: {
+            score: number,
+            categoryName: string
+          }[];
+        }[]
+      }[] = [];
       var classPromise = new Promise<void>((resolve) => {
         this.readAudio(fileList, (buffer) => {
           // let audioClassifier: AudioClassifier;
@@ -124,20 +134,48 @@ export class SimfileGeneratorComponent implements AfterViewInit {
           //   });
           // });
 
-          
-          // const modelUrl = 'https://tfhub.dev/google/tfjs-model/yamnet/tfjs/1';
-          // loadGraphModel(modelUrl, { fromTFHub: true }).then((model)=>{
-          //   const waveform = zeros([16000 * 3]);
-            
-          //   const [scores, embeddings, spectrogram] = model.predict(waveform) as Tensor<Rank>[];
-          //   scores.print(true);  // shape [N, 521]
-          //   embeddings.print(true);  // shape [N, 1024]
-          //   spectrogram.print(true);  // shape [M, 64]
-          //   // Find class with the top score when mean-aggregated across frames.
-          //   scores.mean(0).argMax().print(true);
-          //   // Should print 494 corresponding to 'Silence' in YAMNet Class Map.
-          // })
 
+          const audioCtx = new AudioContext();
+          audioCtx.decodeAudioData(buffer).then((audioBuffer) => {
+            var data = audioBuffer.getChannelData(0);
+            const chunkSize = audioBuffer.sampleRate;
+
+            // const modelUrl = 'https://tfhub.dev/google/tfjs-model/yamnet/tfjs/1';
+            tf.loadGraphModel("/assets/ML/yamnet/model.json", {}/*, { fromTFHub: true }*/).then((model) => {
+
+              for (let i = 0; i < data.length; i += chunkSize) {
+                const chunk = data.slice(i, i + chunkSize);
+
+                //const waveform = zeros([16000 * 3]);
+                const waveform = tf.tensor(chunk, [chunk.length]);
+                const [scores, embeddings, spectrogram] = model.predict(waveform) as tf.Tensor<tf.Rank>[];
+                // scores.print(true);  // shape [N, 521]
+                // embeddings.print(true);  // shape [N, 1024]
+                // spectrogram.print(true);  // shape [M, 64]
+                // // Find class with the top score when mean-aggregated across frames.    
+                // scores.mean(0).argMax().print(true);
+                var values: number[] = scores.mean(0).arraySync() as number[];
+                
+
+                // const mask = tf.greaterEqual(scoresMean, 0.1);
+                // const filtered = tf.booleanMaskAsync(scoresMean, mask);
+                // filtered.print(); // prints [0.5, 0.8, 0.9, 0.7, 0.6]
+                // var values: scores
+                // Should print 494 corresponding to 'Silence' in YAMNet Class Map.    
+                // results[0].timestampMs = (i) * 1000 / audioBuffer.sampleRate
+                classResults.push({
+                  classifications: [{
+                    categories: values.map((x, i) => ({
+                      categoryName: `${YamNetTypes[i] ?? "Unknown"}(${x.toFixed(2)})`,
+                      score: x
+                    })).sort((a, b) => b.score - a.score).filter(x=>x.score > 0.01)
+                  }],
+                  timestampMs: (i) * 1000 / audioBuffer.sampleRate
+                });
+              }
+              resolve();
+            });
+          });
 
         });
       });
@@ -174,8 +212,8 @@ export class SimfileGeneratorComponent implements AfterViewInit {
           var label = "";
           for (let index = 0; index < result.classifications[0].categories.length; index++) {
             const element = result.classifications[0].categories[index];
-            if (element.score == 0)
-              break;
+            // if (element.score == 0)
+            //   break;
             label += "; " + element.categoryName;
           }
 
@@ -245,3 +283,4 @@ export class SimfileGeneratorComponent implements AfterViewInit {
     reader.readAsArrayBuffer(file);
   }
 }
+
